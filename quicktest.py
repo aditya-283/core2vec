7796af0503a68fbacf714faa2293e885972ea26f
Aditya Bhagwat <aditya@Adityas-MacBook-Pro.local>
2020-05-27 16:35:37 +0530

    Cleanup codebase

--- quicktest.py
+++
@@ -1,5 +0,0 @@
-Python 2.7.13 (default, Dec 18 2016, 07:03:39) 
-[GCC 4.2.1 Compatible Apple LLVM 8.0.0 (clang-800.0.42.1)] on darwin
-Type "copyright", "credits" or "license()" for more information.
->>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
-Visit http://www.python.org/download/mac/tcltk/ for current information.
